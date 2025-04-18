a4783a8ac7d68f6ed83c8153ad030686d5f37748
mkarpu <mallireddy97k@gmail.com>
2025-04-18 14:32:22 -0500

    More comments from feature

--- Consumer-Service/src/main/java/com/example1/firstkafkaconsumer/FirstKafkaServiceConsumer/KafkaServiceConsumer.java
+++ Consumer-Service/src/main/java/com/example1/firstkafkaconsumer/FirstKafkaServiceConsumer/KafkaServiceConsumer.java
@@ -8,6 +8,7 @@ public class KafkaServiceConsumer {
 
     //adding the comments
     //New comments from feature branch
+    //few more
     @KafkaListener(topics = "first-config-topic-1", groupId = "my-consumer-group")
     public void consumeMessage(String message) {
         System.out.println("Consumed this new message: " + message);
